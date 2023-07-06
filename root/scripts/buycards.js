a51af5e64dfe5ce92546575259a88b6673663e27
anfecabo <campuzano232@hotmail.com>
2023-07-06 16:19:08 -0500

    fetch with async, cards style

--- root/scripts/buycards.js
+++ root/scripts/buycards.js
@@ -1,21 +1,35 @@
 const containercard =  document.querySelector(".container-card");
 
 
-function fetchPokemon(id){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(res => res.json())
-    .then(data => {
-        createcard(data);
-        console.log(data);
-    });
-    
-    
+// function fetchPokemon(id){
+//     console.log(id);
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//     .then(res => res.json())
+//     .then(data => {
+//         createcard(data);
+//         console.log(data);
+//     });
+// }
+async function fetchPokemon(id) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+    console.log(data);
+    createcard(data);
 }
-function fetchPokemons(number){
+
+async function fetchPokemons(number){
     for (let i = 1; i <= number; i++){
-        fetchPokemon(i);
+        await fetchPokemon(i);
     }
 }
+
+// function fetchPokemons(number) {
+//   for (let i = 1; i <= number; i++) {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+//     .then(res => res.json())
+//     .then(res => createcard(res));
+//   }
+// }
 
 function createcard(pokemon){
     const card = document.createElement('div');
@@ -33,21 +47,29 @@ function createcard(pokemon){
     sprite.src = pokemon.sprites.other.dream_world.front_default
     spriteContainer.appendChild(sprite);
 
+    const cardftt = document.createElement('div');
+    cardftt.classList.add('cardftt');
+    
     const experience = document.createElement('p');
     experience.classList.add('exp');
     experience.textContent = pokemon.base_experience
 
-    /*const number = document.createElement('p');
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;*/
+    const btnbuy = document.createElement('button');
+    btnbuy.classList.add('buy');
+    btnbuy.innerText ='Buy'
+  
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
 
     const expcontainer = document.createElement('div');
     expcontainer.classList.add('exp');
 
     card.appendChild(name);
     card.appendChild(spriteContainer);
-    /*card.appendChild(number);*/
-    card.appendChild(experience);
-
+    card.appendChild(number);
+    card.appendChild(cardftt);
+    cardftt.appendChild(experience);
+    cardftt.appendChild(btnbuy);
     containercard.appendChild(card);
 }
-fetchPokemons(16);
+fetchPokemons(8);
