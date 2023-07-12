18f4eae4be371746dd297464eea226e4a7b37e6f
anfecabo <campuzano232@hotmail.com>
2023-07-12 16:11:23 -0500

    dark mode bc

--- root/scripts/buycards.js
+++ root/scripts/buycards.js
@@ -1,27 +1,29 @@
+//Variable para aumentar el número de cartas
+let offset = 0;
+//Define el lugar donde se pintaran las cartas en el HTML
 const containercard =  document.querySelector(".container-card");
+
+//Consulta la API, crea Json y proporciona los datos para crear la carta
 async function fetchPokemon(id) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await res.json();
     createcard(data);
 }
-
-async function fetchPokemons(number){
-    for (let i = 1; i <= number; i++){
+//Recorre los ID de pokemon para pintarlos uno por uno con un límite de 20
+async function fetchPokemons(){
+    const from = 1 + offset;
+    const to = 20 + offset;
+    for (let i = from; i <= to; i++){
         await fetchPokemon(i);
     }
 }
-
+//Cuenta cuantas cartas hay en pantalla
 function numberOfCards(){
     const numCards = document.querySelectorAll('.pokemon-block').length
     const cantidad = document.querySelector('.morec');
     cantidad.textContent = `${numCards} cards`;
-    //const bttMore = document.getElementById('showMore').addEventListener('click',fetchpokemons());
 }
-
-function moreCards(){
-    fetchPokemons+numCards
-}
-
+//crea plantilla para las cartas y al final muestra la cantidad
 function createcard(pokemon){
     const card = document.createElement('div');
     card.classList.add('pokemon-block');
@@ -71,11 +73,24 @@ function createcard(pokemon){
     cardftt.appendChild(btnbuy);
     containercard.appendChild(card);
 
-}
-
-(async () => {
-    await fetchPokemons(20);
     numberOfCards();
+}
+// Agrega 20 cartas a la función fetchpokemos
+function addMoreCards(){
+    offset += 20;
+    fetchPokemons();
+}
+//Configura el botón ver mas y al final llama a la función fetchpokemos para agregar las cartas.
+(async () => {
+    const btnMore = document.querySelector('#showMore');
+    btnMore.addEventListener('click', addMoreCards);
+
+    await fetchPokemons();
 })();
 
+//modo oscuro
 
+function toggleTheme(){
+    document.body.classList.toggle('dark');
+    document.getElementById("titulo").classList.toggle('color');
+}
