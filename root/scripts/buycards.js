ab815d2f0cc6f7a5b59204cd98bfba8a1c813455
anfecabo <campuzano232@hotmail.com>
2023-07-25 17:05:03 -0500

    Button Up, Sort Arrat favorites

--- root/scripts/buycards.js
+++ root/scripts/buycards.js
@@ -65,7 +65,24 @@ function createcard(pokemon){
 
     const heart = document.createElement('div');
     heart.classList.add('root');
-    heart.innerHTML = `
+    if(pokemon.id == fav.filter(fav => fav == pokemon.id)){
+        heart.innerHTML = `
+    <label id="cbx${pokemon.id.toString()}" onclick="hiden(${pokemon.id.toString()})">
+      <span class="heart" >
+        <svg class="unchecked hiden" id="unchecked${pokemon.id.toString()}" viewBox="0 0 24 24">
+          <path 
+            d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"
+          ></path>
+        </svg>
+        <svg class="checked" id="checked${pokemon.id.toString()}"  viewBox="0 0 24 24">
+          <path
+            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+          ></path>
+        </svg>
+      </span>
+    </label>`;
+    }else{
+        heart.innerHTML = `
     <label id="cbx${pokemon.id.toString()}" onclick="hiden(${pokemon.id.toString()})">
       <span class="heart" >
         <svg class="unchecked" id="unchecked${pokemon.id.toString()}" viewBox="0 0 24 24">
@@ -80,6 +97,8 @@ function createcard(pokemon){
         </svg>
       </span>
     </label>`;
+    }
+    
     const sprite =document.createElement('img');
     sprite.classList.add('imagen');
     sprite.src = pokemon.sprites.other['official-artwork'].front_default;
@@ -255,9 +274,9 @@ document.getElementById("favoritos").addEventListener('click', function(ev) {
 }, false);
 
 //
-function fetchFav(favoritos){
-    favoritos.forEach(element => {
-        fetchPokemon(element);
+async function fetchFav(favoritos){
+    await favoritos.forEach(element => {
+         fetchPokemon(element);
     });  
 }
 
@@ -267,20 +286,33 @@ function hiden(id){
     document.getElementById("checked" + id).classList.toggle("hiden");
     if (document.getElementById("unchecked" + id).classList == "unchecked hiden"){
         fav.push(id);
+        fav = fav.sort(function(a, b){return a - b});
     }
     else{
         fav = fav.filter(fav => fav != id );
+        fav = fav.sort(function(a, b){return a - b});
     }
     console.log(fav);
 }
 
 //Ir arriba
-(document).ready(function(){ irArriba(); });
+buttonUp = document.getElementById('goUp')
 
-function irArriba(){
-  ('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
-  (window).scroll(function(){
-    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
-  });
-  ('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+buttonUp.addEventListener('click', scrollUp);
+
+function scrollUp(bu){
+    const courrentScroll = document.documentElement.scrollTop;
+    if (courrentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0,courrentScroll-(courrentScroll/10));
+    }
+}
+
+window.onscroll = function(ec){
+    const scroll = document.documentElement.scrollTop;
+    if(scroll > 1200){
+        buttonUp.style.display =('block');
+    } else{
+        buttonUp.style.display =('none');
+    }
 }
